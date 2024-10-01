c0f168a02f510260472267a91fb1e94014958859
Nicolas <Nicolas.rocca10@gmail.com>
2024-10-01 01:19:42 -0300

    Bandas Actualizado

--- src/main/java/corsi/prog2/ministerio/controlador/ABMBandas.java
+++ src/main/java/corsi/prog2/ministerio/controlador/ABMBandas.java
@@ -2,12 +2,14 @@ package corsi.prog2.ministerio.controlador;
 
 import corsi.prog2.ministerio.modelo.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Controller
 public class ABMBandas {
 
     @Autowired
